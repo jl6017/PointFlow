027f32787cac3cbc1ce29e390f0bf0a204ce8d0b
jl6017 <jl6017@columbia.edu>
2025-09-12 20:50:43 -0400

    gentraining, distance issue

--- scripts/shapenet_airplane_gen.sh
+++ scripts/shapenet_airplane_gen.sh
@@ -28,7 +28,7 @@ python train.py \
     --epochs ${epochs} \
     --use_adjoint True \
     --rtol 1e-3 --atol 1e-3 \
-    --save_freq 50 --viz_freq 1 --log_freq 1 --val_freq 10 \
+    --save_freq 10 --viz_freq 1 --log_freq 1 --val_freq 10 \
     --use_latent_flow
 
 echo "Done"
