e75d9e3ac28ceb9e85d18e691421a24e7c8a1f5a
jl6017 <jl6017@columbia.edu>
2025-09-16 22:19:28 -0400

    demo4096

--- scripts/robot_demo.sh
+++ scripts/robot_demo.sh
@@ -3,9 +3,9 @@ data_dir="./../dataset_cr/pointcloud_15k/"
 python demo_cr.py \
     --cates airplane \
     --data_dir ${data_dir} \
-    --resume_checkpoint checkpoints/gen/robot-caterobot/checkpoint-latest.pt \
+    --resume_checkpoint checkpoints/gen1/robot-caterobot/checkpoint-latest.pt \
     --dims 512-512-512 \
     --latent_dims 256-256 \
     --use_latent_flow \
     --num_sample_shapes 20 \
-    --num_sample_points 1024
+    --num_sample_points 4096
